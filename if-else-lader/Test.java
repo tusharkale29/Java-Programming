2e85ac0be1e2de9a0b415d50ef3fd3873c2ea49b
TUSHAR KALE <105812893+tusharkale29@users.noreply.github.com>
2023-04-05 21:00:13 +0530

    Add files via upload

---
+++ if-else-lader/Test.java
@@ -0,0 +1,19 @@
+public class Test
+{
+	public static void main(String args[])
+	{
+		int no=0;
+		if(no>0)
+		{
+			System.out.println("+ve no");
+		}
+		else if(no<0)
+		{
+			System.out.println("-ve no");
+		}
+		else
+		{
+			System.out.println("no is zero");
+		}
+	}
+}
