f0fbdd959cf384c3c9590a1ceb443fc12d752c63
TUSHAR KALE <105812893+tusharkale29@users.noreply.github.com>
2023-04-05 21:01:57 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,14 @@
+public class Factorial
+{
+	public static void main(String args[])
+	{
+		int n=5;
+		int fact=1;
+		
+		for(int i=1;i<=n;i++)
+		{
+			fact=fact*i;
+		}
+		System.out.println(" factorial of no:" + fact);
+	}
+}
