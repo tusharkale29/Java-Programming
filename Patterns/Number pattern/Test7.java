59635776b936290c9908cef29a9457e91ecbecba
TUSHAR KALE <105812893+tusharkale29@users.noreply.github.com>
2023-04-07 21:52:30 +0530

    Add files via upload

---
+++ Patterns/Number pattern/Test7.java
@@ -0,0 +1,14 @@
+public class Test7
+{
+	public static void main(String args[])
+	{
+		for(int i=1;i<=4;i++)
+		{
+			for(int j=4;j>=i;j--)
+			{
+				System.out.print( );
+			}
+			System.out.println();
+		}	
+	}
+}
---
+++ Patterns/Start pattern/Test4.java
@@ -0,0 +1,24 @@
+public class Test4
+{
+	public static void main(String args[])
+	{
+		for(int i=1;i<=4;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+			
+			for(int k=1;k<=4;k++)	
+			{
+				for(int j=4;j>=k;j++)
+				{
+					System.out.print("*");
+				}
+				System.out.println();
+			}
+		}
+		
+	}
+}
